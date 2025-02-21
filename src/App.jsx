2e577d2f0b1301db1300de752c8855af83111c23
Anthony <nitinbhagatxyz2@gmail.com>
2025-02-21 11:55:45 +0530

    frontend added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import DashboardLayoutBasic from './components/Drawer';
+
+
+const App = () => {
+  return (
+    <DashboardLayoutBasic />
+  )
+}
+
+export default App;
